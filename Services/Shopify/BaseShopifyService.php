c72a151ab9fca8954c78b5b950f19781cf82594b
l.hirja <l.hirja@sintraconsulting.eu>
2018-05-24 16:30:29 +0200

    Created insert single value for shopify services

--- Services/Shopify/BaseShopifyService.php
+++ Services/Shopify/BaseShopifyService.php
@@ -1,28 +1,56 @@
 <?php
 
-namespace SintraPimcoreBundle\Services;
+namespace SintraPimcoreBundle\Services\Shopify;
+
+use SintraPimcoreBundle\Services\BaseEcommerceService;
 
 abstract class BaseShopifyService extends BaseEcommerceService {
     protected function insertSingleValue (&$ecommObject, $fieldName, $fieldvalue, $isBrick = false) {
         if ($isBrick) {
-            $ecommObject["custom_attributes"][] = array(
-                    "attribute_code" => $fieldName,
-                    "value" => $fieldvalue
-            );
             return;
         }
 
         if (array_key_exists($fieldName, $ecommObject)) {
             $ecommObject[$fieldName] = $fieldvalue;
-        } else if ($ecommObject["attribute_code"] == $fieldName) {
-            $ecommObject["value"] = $fieldvalue;
         } else {
-            //recursion
-            foreach ($ecommObject as $key => $field) {
-                if (is_array($field)) {
-                    $this->insertSingleValue($ecommObject[$key], $fieldName, $fieldvalue);
-                }
+            $shopifyField = '';
+            switch ($fieldName) {
+                case "name":
+                    $shopifyField = 'title';
+                    break;
+                case "status":
+                    $fieldvalue = (boolean)$fieldvalue;
+                    $shopifyField = 'published';
+                    break;
+                case "description":
+                    $shopifyField = 'body_html';
+                    break;
+                case "sku":
+                case "price":
+                case "weight":
+                    $shopifyField = 'variants';
+                    break;
+                default :
+                    $shopifyField = 'metafields';
             }
+            $this->parseField($ecommObject, $shopifyField, $fieldName, $fieldvalue);
+        }
+    }
+
+    protected function parseField (&$ecommObject, $shopifyField, $origField, $origValue) {
+        if ($shopifyField == 'variants') {
+            $ecommObject['variants'][0][$origField] = $origValue;
+        } else if ($shopifyField == 'metafields') {
+            if (!in_array($origField, $this->productExportHidden)) {
+                $ecommObject['metafields'][] = [
+                        "key" => $origField,
+                        "value" => $origValue,
+                        "value_type" => "string",
+                        "namespace" => "global"
+                ];
+            }
+        } else {
+            $ecommObject[$shopifyField] = $origValue;
         }
     }
 }

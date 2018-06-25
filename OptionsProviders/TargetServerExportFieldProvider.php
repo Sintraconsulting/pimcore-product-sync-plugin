1db5ee078bd3096111d161e46d35c032ce802a6c
Marco Guiducci <m.guiducci@sintraconsulting.eu>
2018-06-25 18:32:22 +0200

    magement of localized fields and relations

--- OptionsProviders/TargetServerExportFieldProvider.php
+++ OptionsProviders/TargetServerExportFieldProvider.php
@@ -2,7 +2,6 @@
 
 namespace SintraPimcoreBundle\OptionsProviders;
 
-use Pimcore\Model\DataObject;
 use Pimcore\Model\DataObject\ClassDefinition\DynamicOptionsProvider\MultiSelectOptionsProviderInterface;
 use Pimcore\Model\DataObject\ClassDefinition;
 
@@ -13,19 +12,46 @@ use Pimcore\Model\DataObject\ClassDefinition;
  */
 class TargetServerExportFieldProvider implements MultiSelectOptionsProviderInterface{
 
+    public function hasStaticOptions($context, $fieldDefinition): bool {
+        return true;
+    }
+    
     public function getOptions($context, $fieldDefinition): array {
         $fields = array();
         
-        $productClass = ClassDefinition::getByName("Product");
-        foreach ($productClass->getFieldDefinitions() as $fieldDefinition) {
-            $fields[] = array("key" => $fieldDefinition->getTitle(), "value" => $fieldDefinition->getName());
-        }
+        $classDefinition = ClassDefinition::getByName("Product");
+        $this->extractClassField($classDefinition, $fields);
         
         return $fields;
     }
 
-    public function hasStaticOptions($context, $fieldDefinition): bool {
-        return true;
+    private function extractClassField($classDefinition, &$fields, $isClassRelated = false){
+        foreach ($classDefinition->getFieldDefinitions() as $fieldDefinition) {
+            switch ($fieldDefinition->getFieldtype()){
+                case "localizedfields":
+                    foreach($fieldDefinition->getChilds() as $localizedFieldDefinition){
+                        $fields[] = $this->extractSingleOption($localizedFieldDefinition, $classDefinition, $isClassRelated);
+                    };
+                    break;
+                    
+                case "fieldcollections":
+                    break;
+            
+                default:
+                    $fields[] = $this->extractSingleOption($fieldDefinition, $classDefinition, $isClassRelated);
+                    break;
+            }
+        }
+    }
+    
+    private function extractSingleOption($fieldDefinition, $classDefinition, $isClassRelated){
+        $key = $fieldDefinition->getTitle();
+        $value = $fieldDefinition->getName();
+        
+        return array(
+            "key" => $isClassRelated ? $classDefinition->getName()." - ".$key : $key,
+            "value" => $isClassRelated ? $classDefinition->getName()."__".$value : $value
+        );
     }
 
 }

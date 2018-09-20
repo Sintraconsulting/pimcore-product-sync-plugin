d49e2010a7b5db8453bda34c29b7772a1cd68096
PC-MARCO\mguid <m.guiducci@sintraconsulting.eu>
2018-09-20 16:06:17 +0200

    table install

---
+++ Installer/SintraPimcoreBundleInstaller.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace SintraPimcoreBundle\Installer;
+
+use Doctrine\DBAL\Migrations\Version;
+use Doctrine\DBAL\Schema\Schema;
+use Pimcore\Extension\Bundle\Installer\MigrationInstaller;
+
+class SintraPimcoreBundleInstaller extends MigrationInstaller{
+    
+    public function migrateInstall(Schema $schema, Version $version) {
+        $version->addSql("CREATE TABLE IF NOT EXISTS `custom_log` (
+            `id` INT NOT NULL AUTO_INCREMENT,
+            `gravity` VARCHAR(45) NOT NULL,
+            `action` VARCHAR(255) NOT NULL,
+            `description` TEXT NOT NULL,
+            `timestamp` TIMESTAMP NOT NULL,
+            PRIMARY KEY (`id`));"
+        );
+    }
+
+    public function migrateUninstall(Schema $schema, Version $version) {
+        
+    }
+
+}

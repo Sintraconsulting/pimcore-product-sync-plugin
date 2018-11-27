aa0a150a7ae3c77e689e2a47fff6d324655ff733
PC-MARCO\mguid <m.guiducci@sintraconsulting.eu>
2018-11-27 18:38:00 +0100

    comments and refactoring for controller

--- Controller/SintraPimcoreController.php
+++ Controller/SintraPimcoreController.php
@@ -2,7 +2,6 @@
 
 namespace SintraPimcoreBundle\Controller;
 
-use SintraPimcoreBundle\Controller\Sync\BaseSyncController;
 use Pimcore\Bundle\AdminBundle\Controller\AdminControllerInterface;
 use Pimcore\Cache;
 use Pimcore\Controller\Controller;
@@ -12,15 +11,16 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use SintraPimcoreBundle\Utils\GeneralUtils;
+use SintraPimcoreBundle\Utils\SynchronizationUtils;
 use Pimcore\Db;
-
 use SintraPimcoreBundle\Resources\Ecommerce\BaseEcommerceConfig;
 
-
 /**
- * Class SintraPimcoreController
- * @package SintraPimcoreBundle\Controller
+ * Controller for SintraPimcoreBundle
+ * Provide the method to synchronize objects on external servers
  *
+ * @author Sintra Consulting
+ * 
  * @Route("/sintra_pimcore")
  */
 class SintraPimcoreController extends Controller implements AdminControllerInterface {
@@ -28,35 +28,59 @@ class SintraPimcoreController extends Controller implements AdminControllerInter
     /**
      * @inheritDoc
      */
-    public function needsSessionDoubleAuthenticationCheck()
-    {
+    public function needsSessionDoubleAuthenticationCheck() {
         return false;
     }
 
     /**
      * @inheritDoc
      */
-    public function needsStorageDoubleAuthenticationCheck()
-    {
+    public function needsStorageDoubleAuthenticationCheck() {
         return false;
     }
 
     /**
-     * Syncronize objects in all enabled servers
-     *
+     * Synchronize objects in one or more servers
+     * 
+     * @param Request $request the action request. It could have these arguments:
+     *      - class (mandatory):  a valid Classname, to be choosen from the 
+     *                            Pimcore created classes.
+     * 
+     *      - server:             if passed, the server in which object must be synchronized. 
+     *                            If not passed, all enabled servers will be considered.
+     * 
+     *      - limit (default 10): the number of object to synchronize per iteration.
+     * 
+     *      - execTime:           if passed, specify the maximum execution time (in seconds).
+     *                            in combination with 'maxSyncTime' and 'typicalSyncTime'
+     *                            override the limit parameter
+     * 
+     *      - maxSyncTime:        if passed, specify the empiric maximum synchronization time for a single object (in seconds).
+     *                            in combination with 'execTime' and 'typicalSyncTime'
+     *                            override the limit parameter
+     * 
+     *      - typicalSyncTime:    if passed, specify the empiric typicall synchronization time for a single object  (in seconds).
+     *                            in combination with 'execTime' and 'maxSyncTime'
+     *                            override the limit parameter
+     *       
+     * @return Response The synchronization result
+     * @throws \Exception
+     * 
      * @Route("/sync_objects")
      */
-    public function syncObjectsAction(Request $request)
-    {
-        set_time_limit(480);
-
+    public function syncObjectsAction(Request $request) {
         $class = $request->get("class");
         $availableClasses = GeneralUtils::getAvailableClasses();
 
-        if(!in_array($class, $availableClasses)){
-            throw new \Exception("Invalid class '".$class."'. Please choose a value in ['". implode("','", $availableClasses)."']");
+        if (!in_array($class, $availableClasses)) {
+            throw new \Exception("Invalid class '" . $class . "'. Please choose a value in ['" . implode("','", $availableClasses) . "']");
         }
 
+        /**
+         * Build the custom filter with timing from the request
+         * They will be used to properly define a valid limit
+         * of object to synchronize at each iteration.
+         */
         $customFilters = [];
         $execTime = $request->get('execTime');
         $maxSyncTime = $request->get('maxSyncTime');
@@ -67,86 +91,90 @@ class SintraPimcoreController extends Controller implements AdminControllerInter
             $customFilters += ['typicalSyncTime' => $typicalSyncTime];
         }
 
-        $customizationInfo = BaseEcommerceConfig::getCustomizationInfo();
-        $namespace = $customizationInfo["namespace"];
-
         $response = [];
 
-        $semaphore = __DIR__ ."/syncronization.lock";
-        if(!file_exists($semaphore)){
+        /**
+         * Add a semaphore control in order to avoid concurrent synchronizations
+         * Perform the object synchronization and return the results.
+         * 
+         * If an unexpected error occours, it will be reported in the custom log table
+         * in the database
+         */
+        $semaphore = __DIR__ . "/synchronization.lock";
+        if (!file_exists($semaphore)) {
             $file = fopen($semaphore, "w");
 
             try {
+                $this->doObjectsSynchronization($request, $response, $class, $customFilters);
 
-                if ($namespace) {
-                    $ctrName = $namespace . '\SintraPimcoreBundle\Controller\Sync\CustomBaseSyncController';
-                }
-
-                if($ctrName != null && class_exists($ctrName)){
-                    $syncCTRClass =  new \ReflectionClass($ctrName);
-                    $syncCTR = $syncCTRClass->newInstance();
-                }else {
-                    $syncCTR = new BaseSyncController();
-                }
-
-                $servers = new TargetServer\Listing();
-
-                if($request->get("server") != null && !empty($request->get("server"))){
-                    $servers->setCondition("o_key = ?",$request->get("server"));
-                }else{
-                    $servers = $syncCTR->getEnabledServers();
-                }
-
-                $limit = $request->get("limit");
-
-                foreach ($servers->getObjects() as $server) {
-                    if($limit != null && !empty($limit) && (ctype_digit($limit) || is_int($limit))){
-                        $response[] = ($syncCTR->syncServerObjects($server, $class, $limit, $customFilters));
-                    }else{
-                        $response[] = ($syncCTR->syncServerObjects($server, $class, 10, $customFilters));
-                    }
-                }
-
-                Cache::clearTag("output");
+                Cache::clearAll();
 
                 fclose($file);
                 unlink($semaphore);
-
             } catch (\Error $e) {
                 fclose($file);
                 unlink($semaphore);
-                
-                $db = Db::get();
-                $db->insert(BaseEcommerceConfig::getCustomLogTableName(), array(
-                    "gravity" => "HIGH",
-                    "class" => "SintraPimcoreController",
-                    "action" => "syncObjectsAction",
-                    "flow" => "$class Syncronization",
-                    "description" => $e->getMessage(),
-                    "timestamp" => time()
-                ));
 
-                Logger::err($e->getMessage());
+                $this->logSynchronizationError($class, $e->getMessage());
                 echo $e->getMessage();
             } catch (\Exception $e) {
                 fclose($file);
                 unlink($semaphore);
-                
-                $db = Db::get();
-                $db->insert(BaseEcommerceConfig::getCustomLogTableName(), array(
-                    "gravity" => "HIGH",
-                    "class" => "SintraPimcoreController",
-                    "action" => "syncObjectsAction",
-                    "flow" => "$class Syncronization",
-                    "description" => $e->getMessage(),
-                    "timestamp" => time()
-                ));
 
-                Logger::err($e->getMessage());
+                $this->logSynchronizationError($class, $e->getMessage());
                 echo $e->getMessage();
             }
         }
 
-        return new Response(implode('<br>'.PHP_EOL, $response));
+        return new Response(implode('<br>' . PHP_EOL, $response));
     }
+
+    /**
+     * Perfor objects synchronization for a specific class.
+     * If no specific server is requested, 
+     * all enabled servers will be taken into consideration.
+     * 
+     * Set a default limit in number of objects to synchronize
+     * if this value is missing or is invalid from the request.
+     * 
+     * @param Request $request the action request
+     * @param array $response The synchronization result
+     * @param String $class the class of the objects to synchronize
+     * @param array $customFilters cointains synchronization timing frpm the request.
+     */
+    private function doObjectsSynchronization(Request $request, array &$response, $class, $customFilters = []) {
+        $syncCTR = SynchronizationUtils::getBaseSynchronizationController();
+
+        $servers = new TargetServer\Listing();
+
+        if ($request->get("server") != null && !empty($request->get("server"))) {
+            $servers->setCondition("o_key = ?", $request->get("server"));
+        } else {
+            $servers = $syncCTR->getEnabledServers();
+        }
+
+        $limit = $request->get("limit");
+        if ($limit == null || empty($limit) || !ctype_digit($limit) || !is_int($limit)) {
+            $limit = 10;
+        }
+
+        foreach ($servers->getObjects() as $server) {
+            $response[] = ($syncCTR->syncServerObjects($server, $class, $limit, $customFilters));
+        }
+    }
+
+    private function logSynchronizationError($class, $message) {
+        $db = Db::get();
+        $db->insert(BaseEcommerceConfig::getCustomLogTableName(), array(
+            "gravity" => "HIGH",
+            "class" => "SintraPimcoreController",
+            "action" => "syncObjectsAction",
+            "flow" => "$class Syncronization",
+            "description" => $message,
+            "timestamp" => time()
+        ));
+        
+        Logger::err($message);
+    }
+
 }
--- Controller/Sync/BaseSyncController.php
+++ Controller/Sync/BaseSyncController.php
@@ -7,81 +7,62 @@ use Pimcore\Model\DataObject\ClassDefinition;
 use Pimcore\Model\DataObject\TargetServer;
 use SintraPimcoreBundle\Services\InterfaceService;
 use Pimcore\Logger;
-use ReflectionClass;
 use Pimcore\Db;
 use SintraPimcoreBundle\Resources\Ecommerce\BaseEcommerceConfig;
+use SintraPimcoreBundle\Utils\SynchronizationUtils;
 
 class BaseSyncController {
 
     /**
      * Get the list of enabled TargetServer
      *
-     * @return \Pimcore\Model\DataObject\TargetServer\Listing
+     * @return TargetServer\Listing
      */
-    public function getEnabledServers(){
+    public function getEnabledServers() {
         $servers = new TargetServer\Listing();
         $servers->addConditionParam('enabled', true);
 
         return $servers;
     }
 
-
     /**
      * Dispatch syncronization invoking the server related syncronization service
-     *
-     * @param TargetServer $server
-     * @param $class
-     * @throws \ReflectionException
+     * 
+     * @param TargetServer $server the server in which the objects must me synchronized
+     * @param String $class the class of the objects
+     * @param int $limit number of objects to synchronize
+     * @param array $customFilters timing informations for execution. it override limit if present
+     * @return mixed
      */
-    public function syncServerObjects ($server, $class, $limit = 10, $customFilters = []) {
-        $serverType = $server->getServer_type();
+    public function syncServerObjects(TargetServer $server, $class, $limit = 10, $customFilters = []) {
 
-        $customizationInfo = BaseEcommerceConfig::getCustomizationInfo();
-        $namespace = $customizationInfo["namespace"];
-        $ctrName = null;
-        $serviceName = null;
+        $dataObjectService = SynchronizationUtils::getSynchronizationService($server, $class);
 
-        if ($namespace) {
-            $ctrName = $namespace . '\SintraPimcoreBundle\Controller\Sync\\' . ucfirst($serverType) . 'SyncController';
-            $serviceName = $namespace . '\SintraPimcoreBundle\Services\\' . ucfirst($serverType) . '\\' . ucfirst($serverType) . ucfirst($class) . 'Service';
+        $syncController = SynchronizationUtils::getServerSynchronizationController($server);
+        if ($syncController === null) {
+            $syncController = $this;
         }
 
-        $syncController = null;
-        if($ctrName != null && class_exists($ctrName)){
-            $syncControllerClass =  new ReflectionClass($ctrName);
+        $dataObjects = $syncController->getServerToSyncObjects($server, $class, $limit, $customFilters);
 
-            $syncController = $syncControllerClass->newInstance();
-            $dataObjects = $syncController->getServerToSyncObjects($server, $class, $limit, $customFilters);
-        }else {
-            $dataObjects = $this->getServerToSyncObjects($server, $class, $limit);
+        if ($dataObjects != null && !empty($dataObjects)) {
+            return $syncController->exportDataObjects($dataObjectService, $dataObjects, $server, $class, $customFilters);
         }
 
-        if($serviceName == null || !class_exists($serviceName)){
-            $serviceName = "\SintraPimcoreBundle\Services\\" . ucfirst($serverType) . '\\' . ucfirst($serverType) . ucfirst($class) . 'Service';
-        }
-
-        if($dataObjects != null && !empty($dataObjects)){
-            $dataObjectServiceClass = new ReflectionClass($serviceName);
-            $dataObjectService = $dataObjectServiceClass->newInstanceWithoutConstructor();
-            $dataObjectService = $dataObjectService::getInstance();
-
-            return $syncController != null ?
-                    ($syncController->exportDataObjects($dataObjectService, $dataObjects, $server, $class, $customFilters)) :
-                    ($this->exportDataObjects($dataObjectService, $dataObjects, $server, $class));
-        }
-
-        Logger::info("BaseSyncController - There are no $class to sync for '".$server->getServer_name()."' server");
-        return "BaseSyncController - There are no $class to sync for '".$server->getServer_name()."' server";
+        Logger::info("BaseSyncController - There are no $class to sync for '" . $server->getServer_name() . "' server");
+        return "BaseSyncController - There are no $class to sync for '" . $server->getServer_name() . "' server";
     }
 
     /**
-     * get a batch of ids of objects that need to be syncronized in a specific server
+     * Get a batch of ids of objects that need to be syncronized in a specific server
+     * take in consideration all published objects of the requested class
+     * that must be exported and that are completed but not synchronized in the requested server.
      *
      * @param TargetServer $server
      * @param $class
      * @param int $limit
      */
-    public function getServerToSyncObjects (TargetServer $server, $class, $limit) {
+    public function getServerToSyncObjects(TargetServer $server, $class, $limit, $customFilters = []) {
         /**
          * dynamically get syncronization info tablename starting from class definition.
          * take the field collection type from the exportServers field allowed types.
@@ -89,17 +70,20 @@ class BaseSyncController {
         $classDef = ClassDefinition::getByName($class);
         $fieldCollName = $classDef->getFieldDefinition('exportServers')->getAllowedTypes()[0];
         $classId = $classDef->getId();
-        $fieldCollectionTable = 'object_collection_' . $fieldCollName . '_' .$classId;
+        $fieldCollectionTable = 'object_collection_' . $fieldCollName . '_' . $classId;
+
+        if ($customFilters['execTime'] && $customFilters['maxSyncTime'] && $customFilters['typicalSyncTime']) {
+            $limit = floor(($customFilters['execTime'] - $customFilters['maxSyncTime']) / $customFilters['typicalSyncTime']);
+        }
 
         $db = Db::get();
         $objIds = $db->fetchAll(
-            "SELECT dependencies.sourceid FROM dependencies"
-            . " INNER JOIN $fieldCollectionTable as srv ON (dependencies.sourceid = srv.o_id AND srv.name=? AND srv.export = 1 AND srv.complete = 1 AND (srv.sync = 0 OR srv.sync IS NULL))"
-            . " INNER JOIN objects as obj ON (obj.o_id = dependencies.sourceid AND obj.o_className = ? AND obj.o_type = 'object' AND obj.o_published = 1)"
-            . " WHERE dependencies.targetid = ? AND dependencies.targettype LIKE 'object' AND dependencies.sourcetype LIKE 'object'"
-            . " ORDER BY dependencies.sourceid ASC"
-            . " LIMIT $limit",
-            [ $server->getKey(), $class, $server->getId() ]);
+                "SELECT dependencies.sourceid FROM dependencies"
+                . " INNER JOIN $fieldCollectionTable as srv ON (dependencies.sourceid = srv.o_id AND srv.name=? AND srv.export = 1 AND srv.complete = 1 AND (srv.sync = 0 OR srv.sync IS NULL))"
+                . " INNER JOIN objects as obj ON (obj.o_id = dependencies.sourceid AND obj.o_className = ? AND obj.o_type = 'object' AND obj.o_published = 1)"
+                . " WHERE dependencies.targetid = ? AND dependencies.targettype LIKE 'object' AND dependencies.sourcetype LIKE 'object'"
+                . " ORDER BY dependencies.sourceid ASC"
+                . " LIMIT $limit", [$server->getKey(), $class, $server->getId()]);
 
         $ids = [];
         foreach ($objIds as $id) {
@@ -115,14 +99,14 @@ class BaseSyncController {
      * @param TargetServer $server
      * @return string
      */
-    protected function exportDataObjects (InterfaceService $dataObjectService, $dataObjects, TargetServer $server, $class, $customFilters = []) {
+    protected function exportDataObjects(InterfaceService $dataObjectService, $dataObjects, TargetServer $server, $class, $customFilters = []) {
         $response = array(
-                "started" => date("Y-m-d H:i:s"),
-                "finished" => "",
-                "total elements" => 0,
-                "syncronized elements" => 0,
-                "elements with errors" => 0,
-                "errors" => array()
+            "started" => date("Y-m-d H:i:s"),
+            "finished" => "",
+            "total elements" => 0,
+            "syncronized elements" => 0,
+            "elements with errors" => 0,
+            "errors" => array()
         );
 
         $totalElements = 0;
@@ -130,21 +114,34 @@ class BaseSyncController {
         $elementsWithError = 0;
         $startTime = $this->millitime();
 
+        $initialTime = $this->millitime();
+        $hasTimeLimitation = false;
+        if (isset($customFilters['execTime']) && isset($customFilters['maxSyncTime']) && isset($customFilters['typicalSyncTime'])) {
+            $hasTimeLimitation = true;
+        }
+
         foreach ($dataObjects as $productId) {
 
-            try{
+            $currTime = $this->millitime();
+            $actualDuration = $currTime - $initialTime;
+
+            if ($hasTimeLimitation && ($actualDuration >= $customFilters['execTime'] * 1000 - $customFilters['maxSyncTime'] * 1000)) {
+                break;
+            }
+
+            try {
                 $dataObjectService->export($productId, $server);
                 $syncronizedElements++;
-            } catch(\Exception $e){
-                $response["errors"][] = "OBJECT ID ".$productId.": ".$e->getMessage();
+            } catch (\Exception $e) {
+                $response["errors"][] = "OBJECT ID " . $productId . ": " . $e->getMessage();
 
                 $db = Db::get();
                 $db->insert(BaseEcommerceConfig::getCustomLogTableName(), array(
                     "gravity" => "LOW",
                     "class" => "BaseSyncController",
                     "action" => "exportDataObjects",
-                    "flow" => "Sync to ".$server->getServer_name(),
-                    "description" => "ERROR in exporting object with Id '$productId': ".$e->getMessage(),
+                    "flow" => "Sync to " . $server->getServer_name(),
+                    "description" => "ERROR in exporting object with Id '$productId': " . $e->getMessage(),
                     "timestamp" => time()
                 ));
 
@@ -155,12 +152,11 @@ class BaseSyncController {
             }
 
             $totalElements++;
-
         }
         $endTime = $this->millitime();
-        try{
+        try {
             Cache::clearAll();
-        } catch(\Exception $e){
+        } catch (\Exception $e) {
             Logger::err($e->getMessage());
         }
 
@@ -168,21 +164,21 @@ class BaseSyncController {
         $response["syncronized elements"] = $syncronizedElements;
         $response["elements with errors"] = $elementsWithError;
 
-        return $this->logSyncedProducts($response, $server->getServer_name(), $class, $endTime-$startTime, null);
+        return $this->logSyncedProducts($response, $server->getServer_name(), $class, $endTime - $startTime, null);
     }
 
-    protected function logSyncedProducts ($response, $ecomm, $class, $duration, $finished = null) {
+    protected function logSyncedProducts($response, $ecomm, $class, $duration, $finished = null) {
         if (!$finished) {
             $finished = date("Y-m-d H:i:s");
         }
         $response["finished"] = $finished;
 
         $syncLogFile = fopen(PIMCORE_LOG_DIRECTORY . "/syncObjects.log", "a") or die("Unable to open file!");
-        $syncLog = "[" . Date("Y-m-d H:i:s") . "] - ".strtoupper($class)." $ecomm SYNCRONIZATION RESULT: ".print_r(['success' => $response['elements with errors'] == 0, 'responsedata' => $response, 'duration' => $duration . ' ms'],true);
+        $syncLog = "[" . Date("Y-m-d H:i:s") . "] - " . strtoupper($class) . " $ecomm SYNCRONIZATION RESULT: " . print_r(['success' => $response['elements with errors'] == 0, 'responsedata' => $response, 'duration' => $duration . ' ms'], true);
         fwrite($syncLogFile, $syncLog . PHP_EOL);
         fclose($syncLogFile);
 
-        return ("[$finished] - ".strtoupper($class)." $ecomm SYNCRONIZATION RESULT: ".print_r(['success' => $response['elements with errors'] == 0, 'responsedata' => $response , 'duration' => $duration . ' ms'],true).PHP_EOL);
+        return ("[$finished] - " . strtoupper($class) . " $ecomm SYNCRONIZATION RESULT: " . print_r(['success' => $response['elements with errors'] == 0, 'responsedata' => $response, 'duration' => $duration . ' ms'], true) . PHP_EOL);
     }
 
     protected function millitime() {
@@ -193,4 +189,5 @@ class BaseSyncController {
         // in case of "overflow" (PHP converts it to a double)
         return sprintf('%d%03d', $comps[1], $comps[0] * 1000);
     }
+
 }

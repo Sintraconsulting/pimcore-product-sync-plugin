b7c5e09339b96dac0dae7f7b1b2f8ff6266e6f2a
PC-MARCO\mguid <m.guiducci@sintraconsulting.eu>
2019-05-02 11:15:44 +0200

    rename images link operator

---
+++ Import/Operators/FieldCollectionImageLinkOperator.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace SintraPimcoreBundle\Import\Operators;
+
+use Pimcore\DataObject\Import\ColumnConfig\Operator\AbstractOperator;
+use Pimcore\Model\DataObject\Fieldcollection;
+use Pimcore\Model\DataObject\Fieldcollection\Data\ExternalImageInfo;
+use Pimcore\Model\DataObject\Fieldcollection\Data\ImageInfo;
+use Pimcore\Model\DataObject\Data\ExternalImage;
+use Pimcore\Model\Asset\Image;
+
+/**
+ * Operator that add a new external image to the product given the url
+ *
+ * @author Sintra Consulting
+ */
+class FieldCollectionImageLinkOperator extends AbstractOperator {
+
+    private $additionalData;
+
+    public function __construct(\stdClass $config, $context = null) {
+        parent::__construct($config, $context);
+
+        $this->additionalData = json_decode($config->additionalData, true);
+    }
+
+    /**
+     * Get the column value and excape the HTML tags.
+     * Properly set the obtained string to the specific field 
+     * passed as additional data for the operator
+     */
+    public function process($element, &$target, array &$rowData, $colIndex, array &$context = array()) {
+
+        $imageurl = $rowData[$colIndex];
+
+        if (method_exists($target, "getImages") && $this->is_url($imageurl)) {
+            $images = $target->getImages() != null ? $target->getImages() : new Fieldcollection();
+
+            $imageurl = $rowData[$colIndex];
+            $filename = basename($imageurl);
+
+            $classDefinition = ClassDefinition::getByName($target->getClassName());
+            $fieldCollectionType = $classDefinition->getFieldDefinition('images')->getAllowedTypes()[0];
+
+            switch ($fieldCollectionType) {
+                case "ExternalImageInfo":
+                    $this->importExternalImage($target, $images, $imageurl, $filename);
+                    break;
+
+                case "ImageInfo":
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+    
+    private function is_url($string) {
+        $domain = '[a-z0-9]([-a-z0-9]{0,61}[a-z0-9])'; // one domain component //! IDN
+        return preg_match("~^(https?)://($domain?\\.)+$domain(:\\d+)?(/.*)?(\\?.*)?(#.*)?\$~i", $string); //! restrict path, query and fragment characters
+    }
+
+    /**
+     * 
+     * @param Concrete $target
+     * @param Fieldcollection $images
+     * @param string $imageurl
+     * @param string $filename
+     */
+    private function importExternalImage(&$target, $images, $imageurl, $filename) {
+        $externalImage = new ExternalImage();
+        $externalImage->setUrl($imageurl);
+
+        /**
+         * In this phase, just use the file size as image hash.
+         * This may change in future
+         */
+        $headers = get_headers($imageurl, true);
+        $hash = $headers['Content-Length'];
+
+        $imageInfo = $this->retrieveExternalImageInfo($images, $filename);
+
+        if ($imageInfo == null) {
+            $imageInfo = new ExternalImageInfo();
+            $imageInfo->setFilename($filename);
+
+            $imageInfo->setImageurl($externalImage);
+            $imageInfo->setHash($hash);
+            $imageInfo->setLastsync(date("Y-m-d H:i:s"));
+            $imageInfo->setLastupdate(date("Y-m-d H:i:s"));
+
+            $images->add($imageInfo);
+        } else {
+            $imageInfo->setImageurl($externalImage);
+            $imageInfo->setHash($hash);
+            $imageInfo->setLastsync(date("Y-m-d H:i:s"));
+            $imageInfo->setLastupdate(date("Y-m-d H:i:s"));
+        }
+
+        $target->setImages($images);
+    }
+
+    /**
+     * Retrieve product's ImageInfo for the current image if exists
+     *
+     * @param Fieldcollection $productImages
+     * @param String $filename
+     * @return ExternalImageInfo|null
+     */
+    private function retrieveExternalImageInfo($productImages, $filename) {
+        $imageInfo = null;
+
+        foreach ($productImages as $productImageInfo) {
+            if (strtolower($productImageInfo->getFilename()) == strtolower($filename)) {
+                $imageInfo = $productImageInfo;
+                break;
+            }
+        }
+
+        return $imageInfo;
+    }
+
+    private function importImage(){
+        
+    }
+
+}
